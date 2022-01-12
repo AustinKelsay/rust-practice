4f345b37c164d83b5d3760d83e6286831e68bc03
austinkelsay <austinkelsay@yahoo.com>
2022-01-11 20:31:15 -0700

    Simplified code a bit

--- get_bitcoin_price/src/main.rs
+++ get_bitcoin_price/src/main.rs
@@ -2,16 +2,15 @@ extern crate reqwest;
 
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
-    let client = reqwest::Client::new();
-    let res = client.get("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd&include_market_cap=true&include_24hr_change=true").send().await?;
-
+    let res = reqwest::get("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd&include_market_cap=true&include_24hr_change=true")
+        .await?    
+        .text()
+        .await?;
     // If you wanted to print the status or headers
     // println!("Status: {}", res.status());
     // println!("Headers:\n{:#?}", res.headers());
 
-    // Move and borrow value of `res`
-    let body = res.text().await?;
-    println!("\n{}\n", body);
+    println!("\n{}\n", res);
 
     Ok(())
 }
