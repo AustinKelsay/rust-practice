ec17ce86e0732355d8f5e849471a4b211b4e68cf
austinkelsay <austinkelsay@yahoo.com>
2022-01-21 11:52:30 -0700

    More basic practice code

---
+++ rust_basics/variables/target/debug/deps/variables-6d8be22c8ee73e32.d
@@ -0,0 +1,5 @@
+/Users/austinkelsay/Desktop/My-projects/rust-practice/variables/target/debug/deps/variables-6d8be22c8ee73e32: src/main.rs
+
+/Users/austinkelsay/Desktop/My-projects/rust-practice/variables/target/debug/deps/variables-6d8be22c8ee73e32.d: src/main.rs
+
+src/main.rs:
