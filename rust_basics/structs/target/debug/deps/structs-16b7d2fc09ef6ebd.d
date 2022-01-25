66e193c872c55bfa5e917f27e8cd79394a9cb3dd
austinkelsay <austinkelsay@yahoo.com>
2022-01-25 15:55:32 -0700

    Struct examples

---
+++ rust_basics/structs/target/debug/deps/structs-16b7d2fc09ef6ebd.d
@@ -0,0 +1,5 @@
+/Users/austinkelsay/Desktop/My-projects/rust-practice/rust_basics/structs/target/debug/deps/structs-16b7d2fc09ef6ebd: src/main.rs
+
+/Users/austinkelsay/Desktop/My-projects/rust-practice/rust_basics/structs/target/debug/deps/structs-16b7d2fc09ef6ebd.d: src/main.rs
+
+src/main.rs:
---
+++ rust_basics/structs/target/debug/structs.d
@@ -0,0 +1,1 @@
+/Users/austinkelsay/Desktop/My-projects/rust-practice/rust_basics/structs/target/debug/structs: /Users/austinkelsay/Desktop/My-projects/rust-practice/rust_basics/structs/src/main.rs
