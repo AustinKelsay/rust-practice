dcf6eab7881fd3011e7a1cf437d4ef1982abebb0
austinkelsay <austinkelsay@yahoo.com>
2022-03-21 18:49:32 -0600

    Learning more about ownership

---
+++ rust_basics/ownership/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    // OWNERHSIP RULES
+    // 1. Each value in rust has a variable that owns it
+    // 2. Each value can only be owned by one variable
+    // 3. When a variable goes out of scope, the value is dropped
+
+
+    // THE RULES OF REFERENCES
+    // 1. At any given time you can have either one mutable reference or any number of immutable references
+    // 2. References must always be valid
+}
